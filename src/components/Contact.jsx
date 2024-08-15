34ef83542d310b5a8e6eb53f6307b81e67f25013
Ayaz Mirza <37616943+ayazmirza54@users.noreply.github.com>
2024-08-15 10:14:14 +0000

    Correct the env variables for emailjs

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -33,8 +33,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        import.meta.env.EMAILJS_SERVICE_ID,
+        import.meta.env.EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
           to_name: "Ayaz Mirza",
@@ -42,7 +42,7 @@ const Contact = () => {
           to_email: "ayazmirza54@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        import.meta.env.EMAILJS_PUBLIC_KEY
       )
       .then(
         () => {
